51f77b43c62d5b8fac8620c6152250f04546799b
Matthew Guidry <mguid2088@gmail.com>
2019-07-02 16:04:47 -0500

    Add some write performance tests

--- test/test.cpp
+++ test/test.cpp
@@ -1,6 +1,7 @@
 #include <cassert>
 #include <vector>
 #include <string>
+#include <chrono>
 
 #include "csv_io.hpp"
 
@@ -49,9 +50,89 @@ void test_CSVWriter() {
   }
 }
 
+void test_CSVWriterPerf() {
+  std::cout << "Write Performance Test:\n";
+  typedef std::chrono::high_resolution_clock Time;
+
+  std::string filename{"testperf.csv"};
+  CSVWriter csv(filename);
+
+  auto t0 = Time::now();
+  auto t1 = Time::now();
+
+  t0 = Time::now();
+  for (int i = 0; i < 10; i++) {
+    for (int j = 0; j < 5; j++) {
+      csv.push_back(test_values[j]);
+    }
+    csv.write_row();
+    csv.next_row();
+  }
+  t1 = Time::now();
+  std::cout << "10 Row Write: " << std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() << " ns\n";
+
+  t0 = Time::now();
+  for (int i = 0; i < 100; i++) {
+    for (int j = 0; j < 5; j++) {
+      csv.push_back(test_values[j]);
+    }
+    csv.write_row();
+    csv.next_row();
+  }
+  t1 = Time::now();
+  std::cout << "100 Row Write: " << std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() << " ns\n";
+
+  t0 = Time::now();
+  for (int i = 0; i < 1000; i++) {
+    for (int j = 0; j < 5; j++) {
+      csv.push_back(test_values[j]);
+    }
+    csv.write_row();
+    csv.next_row();
+  }
+  t1 = Time::now();
+  std::cout << "1000 Row Write: " << std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() << " ns\n";
+
+  t0 = Time::now();
+  for (int i = 0; i < 10000; i++) {
+    for (int j = 0; j < 5; j++) {
+      csv.push_back(test_values[j]);
+    }
+    csv.write_row();
+    csv.next_row();
+  }
+  t1 = Time::now();
+  std::cout << "10000 Row Write: " << std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() << " ns\n";
+
+  t0 = Time::now();
+  for (int i = 0; i < 100000; i++) {
+    for (int j = 0; j < 5; j++) {
+      csv.push_back(test_values[j]);
+    }
+    csv.write_row();
+    csv.next_row();
+  }
+  t1 = Time::now();
+  std::cout << "100000 Row Write: " << std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() << " ns\n";
+
+  t0 = Time::now();
+  for (int i = 0; i < 1000000; i++) {
+    for (int j = 0; j < 5; j++) {
+      csv.push_back(test_values[j]);
+    }
+    csv.write_row();
+    csv.next_row();
+  }
+  t1 = Time::now();
+  std::cout << "1000000 Row Write: " << std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() << " ns\n";
+
+}
+
 int main () {
   test_SplitFunction();
   test_CSVReader();
   test_CSVWriter();
+
+  test_CSVWriterPerf();
 }
 
