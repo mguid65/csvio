6ce8b23af98cd4fee67cfd036ee983818c351622
Matthew Guidry <mguid2088@gmail.com>
2019-07-03 10:41:51 -0500

    Add some checks and exceptions

--- include/csv_io.hpp
+++ include/csv_io.hpp
@@ -1,19 +1,48 @@
 #include <algorithm>
-#include <iostream>
+#include <cassert>
+#include <exception>
 #include <fstream>
+#include <functional>
+#include <iostream>
+#include <list>
 #include <string>
 #include <string_view>
 #include <vector>
-#include <functional>
+
+template <typename, typename T>
+struct has_push_back {
+  static_assert(
+      std::integral_constant<T, false>::value,
+      "Second template parameter needs to be of function type.");
+};
+
+template <typename C, typename Ret, typename... Args>
+struct has_push_back<C, Ret(Args...)> {
+private:
+  template <typename T>
+  static constexpr auto check(T*) -> typename std::is_same<
+      decltype(std::declval<T>().push_back(std::declval<Args>()...)), Ret>::type;
+
+  template <typename>
+  static constexpr std::false_type check(...);
+
+  typedef decltype(check<C>(0)) type;
+
+public:
+  static constexpr bool value = type::value;
+};
 
 template <typename RowContainer>
 struct SplitFunction {
   static RowContainer delimiter_split(std::string_view input, std::string_view delim) {
+    static_assert(
+        has_push_back<RowContainer, void(const typename RowContainer::value_type&)>::value,
+        "The template parameter needs to be a container type with an push_back function.");
     RowContainer output;
     size_t first = 0;
     while (first < input.size()) {
       const auto second = input.find_first_of(delim, first);
-      if (first != second) output.emplace_back(input.substr(first, second - first));
+      if (first != second) output.push_back(input.substr(first, second - first));
       if (second == std::string_view::npos) break;
       first = second + 1;
     }
@@ -21,35 +50,44 @@ struct SplitFunction {
   }
 };
 
-using VectorSplit = SplitFunction<std::vector<std::string_view>>;
+using VectorSplitSV = SplitFunction<std::vector<std::string_view>>;
+using VectorSplitS = SplitFunction<std::vector<std::string>>;
+using ListSplitSV = SplitFunction<std::list<std::string_view>>;
+using ListSplitS = SplitFunction<std::list<std::string>>;
 
-template <typename RowContainer=std::vector<std::string_view>>
+class ColumnMismatchException : public std::exception {
+  virtual const char* what() const throw() { return "CSV column number mismatch detected"; }
+} ColumnMismatchException;
+
+template <typename RowContainer = std::vector<std::string_view>>
 class CSVReader {
 public:
-
   CSVReader() {}
   CSVReader(
       const std::string& filename, const std::string& delimiter = ",",
       std::ios_base::openmode flags = std::ios::in,
-      std::function<RowContainer(std::string_view, std::string_view)>
-          split_func = VectorSplit::delimiter_split)
+      std::function<RowContainer(std::string_view, std::string_view)> split_func =
+          VectorSplitSV::delimiter_split,
+      bool has_header = false, bool strict_columns = true)
       : m_infile(filename, flags),
         m_filename(filename),
         m_delim(delimiter),
-        m_split_func(split_func) {}
+        m_split_func(split_func),
+        m_has_header(has_header),
+        m_strict_columns(strict_columns) {}
 
-  ~CSVReader() {
-    m_infile.close();
-  }
+  ~CSVReader() { m_infile.close(); }
   void set_delimiter(std::string_view sv) { m_delim = sv; }
   std::string_view get_delimiter() { return m_delim; }
   const std::string_view get_delimiter() const { return m_delim; }
 
   bool good() { return m_infile.good(); }
 
-  RowContainer& next() {
+  RowContainer& current() { return m_current; }
+
+  RowContainer& read() {
     advance();
-    return m_current;
+    return current();
   }
 
 private:
@@ -58,30 +96,52 @@ private:
     parse_current_str();
   }
 
-  void parse_current_str() { m_current = m_split_func(m_current_str_line, m_delim); }
+  void parse_current_str() {
+    if (m_current_str_line.empty()) {
+      m_current.clear();
+      return;
+    }
+    m_current = m_split_func(m_current_str_line, m_delim);
+    if (m_num_columns == -1) {
+      m_num_columns = m_current.size();
+    } else if (m_strict_columns && (m_num_columns != m_current.size())) {
+      throw ColumnMismatchException;
+    }
+  }
+
+  void handle_header() {
+    m_header_names = m_split_func(m_current_str_line, m_delim);
+    m_num_columns = m_header_names.size();
+  }
+
   std::ifstream m_infile;
   std::string_view m_filename;
   std::string_view m_delim;
 
   std::function<RowContainer(std::string_view, std::string_view)> m_split_func;
+  bool m_has_header;
+  bool m_strict_columns;
+
   std::string m_current_str_line;
+  long m_num_columns{-1};
+
+  RowContainer m_header_names;
   RowContainer m_current;
 };
 
-template <typename RowContainer=std::vector<std::string_view>>
+template <typename RowContainer = std::vector<std::string_view>>
 class CSVWriter {
 public:
   CSVWriter() {}
   CSVWriter(
-      const std::string& filename, const std::string& delimiter=",",
-      std::ios_base::openmode flags=std::ios::out)
+      const std::string& filename, const std::string& delimiter = ",",
+      std::ios_base::openmode flags = std::ios::out, bool strict_columns = true)
       : m_outfile(filename, flags),
         m_filename(filename),
-        m_delim(delimiter) {}
+        m_delim(delimiter),
+        m_strict_columns(strict_columns) {}
 
-  ~CSVWriter() {
-    m_outfile.close();
-  }
+  ~CSVWriter() { m_outfile.close(); }
   void set_delimiter(std::string_view sv) { m_delim = sv; }
   std::string_view get_delimiter() { return m_delim; }
   const std::string_view get_delimiter() const { return m_delim; }
@@ -90,58 +150,70 @@ public:
 
   void write_header(const RowContainer& header) {
     if (header.empty()) return;
-    bool first=true;
-    for (auto & s: header) {
-      if(!first) {
+    m_num_columns = header.size();
+    bool first = true;
+    for (auto& s : header) {
+      if (!first) {
         m_outfile << m_delim << s;
       } else {
         m_outfile << s;
         first = false;
       }
     }
-    m_outfile << '\n';
+    m_outfile << "\r\n";
   }
 
   void write_row() {
     if (m_current.empty()) return;
-    bool first=true;
-    for (auto& s: m_current) {
-      if(!first) {
+    if (m_num_columns == -1) {
+      m_num_columns = m_current.size();
+    } else if (m_strict_columns && (m_current.size() != m_num_columns)) {
+      throw ColumnMismatchException;
+    }
+    bool first = true;
+    for (auto& s : m_current) {
+      if (!first) {
         m_outfile << m_delim << s;
       } else {
         m_outfile << s;
         first = false;
       }
     }
-    m_outfile << '\n';
+    m_outfile << "\r\n";
   }
 
   void write_row(const RowContainer& values) {
     if (values.empty()) return;
+    if (m_num_columns == -1) {
+      m_num_columns = values.size();
+    } else if (m_strict_columns && (values.size() != m_num_columns)) {
+      throw ColumnMismatchException;
+    }
     bool first = true;
-    for (auto & s: values) {
-      if(!first) {
+    for (auto& s : values) {
+      if (!first) {
         m_outfile << m_delim << s;
       } else {
         m_outfile << s;
         first = false;
       }
     }
-    m_outfile << '\n';
+    m_outfile << "\r\n";
   }
 
-  void push_back(std::string_view sv) {
-    m_current.push_back(sv);
-  }
+  void write_raw_csv_row(std::string_view raw_csv) { m_outfile << raw_csv << "\r\n"; }
 
-  void next_row() {
-    m_current.clear();
-  }
+  void push_back(std::string_view sv) { m_current.push_back(sv); }
+
+  void next_row() { m_current.clear(); }
 
 private:
   std::ofstream m_outfile;
   std::string_view m_filename;
   std::string_view m_delim;
+
+  bool m_strict_columns;
+  long m_num_columns{-1};
 
   RowContainer m_current;
 };
--- test/test.cpp
+++ test/test.cpp
@@ -1,44 +1,47 @@
 #include <cassert>
-#include <vector>
-#include <string>
 #include <chrono>
+#include <string>
+#include <vector>
 
 #include "csv_io.hpp"
 
-void print_vector(std::vector<std::string_view> v){
-  for (auto & s: v) {
+void print_vector(std::vector<std::string_view> v) {
+  for (auto& s : v) {
     std::cout << s << ' ';
   }
   std::cout << '\n';
 }
 
 void test_SplitFunction() {
+  std::cout << "Split Function Test:\n";
   std::string test_1{"abc,def,ghi,jkl"};
-  std::string delimiter=",";
+  std::string delimiter = ",";
   std::vector<std::string_view> expected{"abc", "def", "ghi", "jkl"};
-  auto actual = VectorSplit::delimiter_split(test_1, delimiter);
+  auto actual = VectorSplitSV::delimiter_split(test_1, delimiter);
 
-  //print_vector(expected);
-  //print_vector(actual);
-  assert(VectorSplit::delimiter_split(test_1, delimiter) == expected);
+  // print_vector(expected);
+  // print_vector(actual);
+  assert(actual == expected);
 }
 
 void test_CSVReader() {
+  std::cout << "Reader Test:\n";
   std::string filename{"test.csv"};
   CSVReader<std::vector<std::string_view>> csv(filename);
   while (csv.good()) {
-    auto v = csv.next();
-    //print_vector(v);
+    auto v = csv.read();
+    // print_vector(v);
   }
 }
 
 std::vector<std::string_view> test_values{"0", "1", "2", "3", "4"};
 
 void test_CSVWriter() {
+  std::cout << "Writer Test:\n";
   std::string filename{"testout.csv"};
   CSVWriter csv(filename);
 
-  csv.write_header({"a","b","c","d","e"});
+  csv.write_header({"a", "b", "c", "d", "e"});
   csv.write_row({"0", "1", "2", "3", "4"});
 
   for (int i = 0; i < 20; i++) {
@@ -69,7 +72,8 @@ void test_CSVWriterPerf() {
     csv.next_row();
   }
   t1 = Time::now();
-  std::cout << "10 Row Write: " << std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() << " ns\n";
+  std::cout << "10 Row Write: "
+            << std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() << " ns\n";
 
   t0 = Time::now();
   for (int i = 0; i < 100; i++) {
@@ -80,7 +84,8 @@ void test_CSVWriterPerf() {
     csv.next_row();
   }
   t1 = Time::now();
-  std::cout << "100 Row Write: " << std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() << " ns\n";
+  std::cout << "100 Row Write: "
+            << std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() << " ns\n";
 
   t0 = Time::now();
   for (int i = 0; i < 1000; i++) {
@@ -91,7 +96,8 @@ void test_CSVWriterPerf() {
     csv.next_row();
   }
   t1 = Time::now();
-  std::cout << "1000 Row Write: " << std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() << " ns\n";
+  std::cout << "1000 Row Write: "
+            << std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() << " ns\n";
 
   t0 = Time::now();
   for (int i = 0; i < 10000; i++) {
@@ -102,7 +108,8 @@ void test_CSVWriterPerf() {
     csv.next_row();
   }
   t1 = Time::now();
-  std::cout << "10000 Row Write: " << std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() << " ns\n";
+  std::cout << "10000 Row Write: "
+            << std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() << " ns\n";
 
   t0 = Time::now();
   for (int i = 0; i < 100000; i++) {
@@ -113,7 +120,8 @@ void test_CSVWriterPerf() {
     csv.next_row();
   }
   t1 = Time::now();
-  std::cout << "100000 Row Write: " << std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() << " ns\n";
+  std::cout << "100000 Row Write: "
+            << std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() << " ns\n";
 
   t0 = Time::now();
   for (int i = 0; i < 1000000; i++) {
@@ -124,15 +132,14 @@ void test_CSVWriterPerf() {
     csv.next_row();
   }
   t1 = Time::now();
-  std::cout << "1000000 Row Write: " << std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() << " ns\n";
-
+  std::cout << "1000000 Row Write: "
+            << std::chrono::duration_cast<std::chrono::nanoseconds>(t1 - t0).count() << " ns\n";
 }
 
-int main () {
+int main() {
   test_SplitFunction();
   test_CSVReader();
   test_CSVWriter();
 
   test_CSVWriterPerf();
 }
-
