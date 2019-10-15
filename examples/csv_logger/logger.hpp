933c11e71d3f796932808da879c89524688b3895
Matthew Guidry <mguid2088@gmail.com>
2019-10-15 16:08:59 -0500

    Weird stuff

--- examples/csv_logger/logger.hpp
+++ examples/csv_logger/logger.hpp
@@ -1,20 +1,21 @@
-#include "csv_io.hpp"
 #include <chrono>
-#include <string>
+#include <cstdio>
+#include <ctime>
+#include <iomanip>
 #include <memory>
 #include <sstream>
-#include <iomanip>
-#include <ctime>
+#include <string>
+#include "csv_io.hpp"
 
 namespace util {
 
 std::string DateTimeNow() {
-    auto now = std::chrono::system_clock::now();
-    auto in_time_t = std::chrono::system_clock::to_time_t(now);
+  auto now = std::chrono::system_clock::now();
+  auto in_time_t = std::chrono::system_clock::to_time_t(now);
 
-    std::stringstream ss;
-    ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d %X");
-    return ss.str();
+  std::stringstream ss;
+  ss << std::put_time(std::localtime(&in_time_t), "%Y-%m-%d %X");
+  return ss.str();
 }
 
 enum LogLevel {
@@ -25,39 +26,44 @@ enum LogLevel {
   FATAL,
 };
 
-std::array<std::string, 5> level_to_string{ "DEBUG", "INFO", "WARN", "ERROR", "FATAL" };
+std::array<std::string, 5> level_to_string{"DEBUG", "INFO", "WARN", "ERROR", "FATAL"};
 
 class Logger {
- public:
+public:
   struct Message : public std::ostringstream {
-    Message(Logger& logger, LogLevel level) : m_logger(logger), m_level(level) {}
-    ~Message() {
-      m_logger.Log({DateTimeNow(), level_to_string[m_level], str()});
-    }
-   private:
-     Logger& m_logger;
-     LogLevel m_level;
+    Message(Logger& logger, LogLevel level, const std::string& location = "")
+        : m_logger(logger), m_level(level), m_location(location) {}
+    ~Message() { m_logger.Log({DateTimeNow(), level_to_string[m_level], m_location, str()}); }
+
+  private:
+    Logger& m_logger;
+    LogLevel m_level;
+    std::string m_location;
   };
 
   explicit Logger(std::ostream& log_sink) {
     m_writer.reset(new csvio::util::CSVLineWriter(log_sink));
     m_csv_writer.reset(new csvio::CSVWriter(*m_writer));
-    m_csv_writer->write_header({"TIME","LEVEL","MESSAGE"});
+    m_csv_writer->write_header({"TIME", "LEVEL", "FUNC", "MESSAGE"});
   }
 
-  Message debug() { return { *this, LogLevel::DEBUG }; }
-  Message info() {return { *this, LogLevel::INFO }; }
-  Message warn() {return { *this, LogLevel::WARN }; }
-  Message error() {return { *this, LogLevel::ERROR }; }
-  Message fatal() {return { *this, LogLevel::FATAL }; }
+  Message debug_(std::string func = "") { return {*this, LogLevel::DEBUG, func}; }
+  Message info_(std::string func = "") { return {*this, LogLevel::INFO, func}; }
+  Message warn_(std::string func = "") { return {*this, LogLevel::WARN, func}; }
+  Message error_(std::string func = "") { return {*this, LogLevel::ERROR, func}; }
+  Message fatal_(std::string func = "") { return {*this, LogLevel::FATAL, func}; }
 
-  void Log(std::initializer_list<std::string> msg) {
-    m_csv_writer->write(msg);
-  }
+#define Debug() debug_(__func__)
+#define Info() info_(__func__)
+#define Warn() warn_(__func__)
+#define Error() error_(__func__)
+#define Fatal() fatal_(__func__)
 
- private:
+  void Log(const std::initializer_list<std::string>& msg) { m_csv_writer->write(msg); }
+
+private:
   std::unique_ptr<csvio::util::CSVLineWriter> m_writer;
   std::unique_ptr<csvio::CSVWriter<std::vector>> m_csv_writer;
 };
 
-} // namespace util
+}  // namespace util
--- examples/csv_logger/test.cpp
+++ examples/csv_logger/test.cpp
@@ -1,12 +1,35 @@
 #include "logger.hpp"
 #include <iostream>
 
+void function_3(util::Logger& log) {
+  log.Debug() << "Hello 3!";
+}
+
+void function_2(util::Logger& log) {
+  log.Debug() << "Hello 2!";
+  function_3(log);
+}
+
+
+void function_1(util::Logger& log) {
+  log.Debug() << "Hello 1!";
+  function_2(log);
+}
+
+
 int main () {
+//  std::ofstream log_file("log_test.csv");
+//  util::Logger log(log_file);
   util::Logger log(std::cout);
 
-  log.debug() << "Hello World!";
-  log.info() << "Hello World!";
-  log.warn() << "Hello World!";
-  log.error() << "Hello World!";
-  log.fatal() << "Hello World!";
+
+/*
+  for (int i = 0; i < 1000000; i++) {
+    log.Debug()() << "Hello World!";
+  }
+*/
+
+  log.Debug() << "Hello World!";
+
+  function_1(log);
 }
